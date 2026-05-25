58a32d6ea05931531e97c6b25ef8a704295b461b
Wes Shi <wes@wesonops.com>
2026-05-25 17:56:29 -0400

    Move reactCompiler to experimental config

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  reactCompiler: true,
+  experimental: {
+    reactCompiler: true,
+  },
 };
 
 export default nextConfig;
